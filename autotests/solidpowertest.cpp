d95f8886d612b9fd472bc608b8e021c3d6bb3cd0
Àlex Fiestas <afiestas@kde.org>
2014-05-09 12:35:09 +0200

    First commit of the Power async api
    Build upon Solid::Job, this api will offer a set of jobs that will
    provide information regarding the power system, some examples:
    -AcPluggedJob
    -SleepJob
    -SleepStatesJob
    ...
    
    The backends at the moment will be in source and hardcoded since we
    don't have time to write a runtime system for it before release. The
    classes though are structured in a way that we will allow backends
    to be outside our source tree in the future.
    
    To enable this feature compile with:
    -DWITH_NEW_POWER_ASYNC_API=On

---
+++ autotests/solidpowertest.cpp
@@ -0,0 +1,44 @@
+/*
+    Copyright 2014 Alejandro Fiestas Olivares <afiestas@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QTest>
+#include <Solid/AcPluggedJob>
+
+using namespace Solid;
+class solidPowerTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testAcPluggedJob();
+};
+
+void solidPowerTest::testAcPluggedJob()
+{
+    AcPluggedJob *job = new AcPluggedJob();
+    QVERIFY(job->exec());
+    QVERIFY(job->isPlugged());
+
+//     AcPluggedJob *job = Solid::Power::acPlugged();
+
+}
+
+QTEST_MAIN(solidPowerTest)
+
+#include "solidpowertest.moc"
---
+++ src/solid/power/acpluggedjob.h
@@ -0,0 +1,48 @@
+/*
+    Copyright 2014 Alejandro Fiestas Olivares <afiestas@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SOLID_AC_PLUGGED_JOB_H
+#define SOLID_AC_PLUGGED_JOB_H
+
+#include <Solid/Job>
+
+namespace Solid
+{
+class AcPluggedJobPrivate;
+class AcPluggedJob : public Job
+{
+    Q_OBJECT
+    Q_PROPERTY(bool plugged READ isPlugged NOTIFY result)
+
+public:
+    explicit AcPluggedJob(QObject *parent = 0);
+
+    virtual void doStart() Q_DECL_OVERRIDE;
+
+    bool isPlugged() const;
+
+Q_SIGNALS:
+    void result(Solid::Job *);
+
+private:
+    Q_DECLARE_PRIVATE(AcPluggedJob)
+};
+} //Solid namespace
+#endif //SOLID_AC_PLUGGED_JOB_H
