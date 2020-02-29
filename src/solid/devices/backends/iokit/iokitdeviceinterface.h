de5a0c3435e8d2cad83ae7bd1c54a31159e399b2
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-02-29 18:34:17 +0100

    Solid: Convert license headers to SPDX expressions
    Summary:
    Convert license headers to SPDX expressions and add
    license files as required by REUSE specification.
    
    Reviewers: cgiboudeaux
    
    Reviewed By: cgiboudeaux
    
    Subscribers: cgiboudeaux, kde-frameworks-devel
    
    Tags: #frameworks
    
    Maniphest Tasks: T11550
    
    Differential Revision: https://phabricator.kde.org/D27742

--- src/solid/devices/backends/iokit/iokitdeviceinterface.h
+++ src/solid/devices/backends/iokit/iokitdeviceinterface.h
@@ -1,22 +1,8 @@
 /*
-    Copyright 2009 Harald Fernengel <harry@kdevelop.org>
-    Copyright 2017 René J.V. Bertin <rjvbertin@gmail.com>
+    SPDX-FileCopyrightText: 2009 Harald Fernengel <harry@kdevelop.org>
+    SPDX-FileCopyrightText: 2017 René J.V. Bertin <rjvbertin@gmail.com>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) version 3, or any
-    later version accepted by the membership of KDE e.V. (or its
-    successor approved by the membership of KDE e.V.), which shall
-    act as a proxy defined in Section 6 of version 3 of the license.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
 #ifndef SOLID_BACKENDS_IOKIT_DEVICEINTERFACE_H
@@ -40,7 +26,7 @@ class DeviceInterface : public QObject, virtual public Solid::Ifaces::DeviceInte
 public:
     DeviceInterface(IOKitDevice *device);
     // the ctor taking a const device* argument makes a deep
-    // copy of the IOKitDevice; any property changes made via 
+    // copy of the IOKitDevice; any property changes made via
     // the resulting instance will not affect the original device.
     DeviceInterface(const IOKitDevice *device);
     virtual ~DeviceInterface();
