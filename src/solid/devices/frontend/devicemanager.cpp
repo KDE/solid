a0019175288ea31e24e8654ddb0dec4ebe94d53f
Marcin Gurtowski <halftough29a@gmail.com>
2020-07-11 16:29:52 +0000

    Removing fileInfo variable, as it's replaced by QFileInfo::exists(path)

--- src/solid/devices/frontend/devicemanager.cpp
+++ src/solid/devices/frontend/devicemanager.cpp
@@ -172,7 +172,6 @@ QList<Solid::Device> Solid::Device::listFromQuery(const Predicate &predicate,
 
 Solid::Device Solid::Device::storageAccessFromPath(QString path)
 {
-    QFileInfo fileInfo = QFileInfo(path);
     if (!QFileInfo::exists(path)) {
         qCWarning(DEVICE_MANAGER_LOG) << "Incorrect file path";
         return Device();
