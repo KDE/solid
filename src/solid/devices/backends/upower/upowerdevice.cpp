4080451c02d9f5b451c91e9dd3a5096dfb977096
Nicolas Fella <nicolas.fella@gmx.de>
2021-11-09 02:14:33 +0100

    [upower] Consider audio devices and generic bluetooth devices to be batteries
    BUG: 445141

--- src/solid/devices/backends/upower/upowerdevice.cpp
+++ src/solid/devices/backends/upower/upowerdevice.cpp
@@ -87,6 +87,10 @@ bool UPowerDevice::queryDeviceInterface(const Solid::DeviceInterface::Type &type
         case UP_DEVICE_KIND_PDA:
         case UP_DEVICE_KIND_PHONE:
         case UP_DEVICE_KIND_GAMING_INPUT:
+        case UP_DEVICE_KIND_SPEAKERS:
+        case UP_DEVICE_KIND_HEADSET:
+        case UP_DEVICE_KIND_HEADPHONES:
+        case UP_DEVICE_KIND_BLUETOOTH_GENERIC:
             return true;
         case UP_DEVICE_KIND_UNKNOWN:
             // There is currently no "Bluetooth battery" type, so check if it comes from Bluez
