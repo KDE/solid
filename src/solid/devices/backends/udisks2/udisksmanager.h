1ee5bba304c12ab218914bb3256dfc726887f0ac
Kai Uwe Broulik <kde@privat.broulik.de>
2024-11-23 21:12:02 +0100

    udisks: Port to DBus Object Manager
    Instead of fetching device properties in the device itself (and
    other places), let UDisks Manager use DBus Object Manager to fetch
    all managed objects and their propertes in one go and then only
    update them as they change.
    
    Device Backend is dropped in favor of having everything contained
    within the Manager, also reduces complexity of having a thread storage
    and static cache of backends, particularly since every thread gets
    its own Manager anyway.
    
    Properties are also stored per-interface and only flattened
    as needed. Simplifies the update code since we can just add/remove
    indvidual interfaces instead of having them all mushed together. They
    are also tracked even for devices that are currently not announced,
    such as removed optical media, so the internal cache is always up-to-date
    further reducing the need of property queries.
    
    Avoid emitting optical discs twice. When it is a data disc, don't
    announce it but wait for its FileSystem interface to appear.
    
    Also moves value sanitation (removal of trailing NUL byte) into
    a dedicated UDisks Utils namespace.
    
    This significantly reduces the amount of blocking DBus calls on
    application startup in e.g. KFilePlacesModel.

--- src/solid/devices/backends/udisks2/udisksmanager.h
+++ src/solid/devices/backends/udisks2/udisksmanager.h
@@ -16,6 +16,8 @@
 
 #include <QSet>
 
+using PropertyMap = QMap<QString, QVariantMap>;
+
 namespace Solid
 {
 namespace Backends
@@ -27,6 +29,11 @@ class Manager : public Solid::Ifaces::DeviceManager
     Q_OBJECT
 
 public:
+    enum FetchMode {
+        FetchIfNeeded,
+        CachedOnly,
+    };
+
     Manager(QObject *parent);
     QObject *createDevice(const QString &udi) override;
     QStringList devicesFromQuery(const QString &parentUdi, Solid::DeviceInterface::Type type) override;
@@ -35,18 +42,29 @@ public:
     QString udiPrefix() const override;
     ~Manager() override;
 
+    bool hasInterface(const QString &udi, const QString &interface);
+    QMap<QString, PropertyMap> allProperties();
+    PropertyMap deviceProperties(const QString &udi);
+    QVariant deviceProperty(const QString &udi, const QString &name, FetchMode fetchMode = FetchIfNeeded);
+
+Q_SIGNALS:
+    void propertyChanged(const QString &udi, const QMap<QString, int> &changes);
+
 private Q_SLOTS:
     void slotInterfacesAdded(const QDBusObjectPath &object_path, const VariantMapMap &interfaces_and_properties);
     void slotInterfacesRemoved(const QDBusObjectPath &object_path, const QStringList &interfaces);
-    void slotMediaChanged(const QDBusMessage &msg);
+    void slotPropertiesChanged(const QDBusMessage &msg);
 
 private:
-    const QStringList &deviceCache();
-    void introspect(const QString &path, bool checkOptical = false);
+    QMap<QString, PropertyMap> deviceCache();
     void updateBackend(const QString &udi);
     QSet<Solid::DeviceInterface::Type> m_supportedInterfaces;
     org::freedesktop::DBus::ObjectManager m_manager;
-    QStringList m_deviceCache;
+
+    // Optical media can be removed from the list of "known" devices
+    // but we still want to cache their properties for when the media is inserted back.
+    QStringList m_devices;
+    QMap<QString, PropertyMap> m_cache;
 };
 
 }
