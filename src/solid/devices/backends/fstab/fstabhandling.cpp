20f33de7c504fa96ee005c76f00f1c09edfbcbfb
Christoph Cullmann <christoph@cullmann.dev>
2025-07-05 10:00:22 +0200

    Revert "handle ZFS in fstab backend as udisk2 has no support"
    This reverts commit 15852ab9da8b09eb75ef1769eae5cc9bbd40a982

--- src/solid/devices/backends/fstab/fstabhandling.cpp
+++ src/solid/devices/backends/fstab/fstabhandling.cpp
@@ -70,11 +70,7 @@ bool _k_isFstabSupportedLocalFileSystem(const QString &fstype)
     if (fstype == QLatin1String("fuse.encfs") //
         || fstype == QLatin1String("fuse.cryfs") //
         || fstype == QLatin1String("fuse.gocryptfs") //
-        || fstype == QLatin1String("overlay")
-
-        // handle ZFS here as udisk2 doesn't support that yet
-        // see https://github.com/storaged-project/udisks/issues/42
-        || fstype == QLatin1String("zfs")) {
+        || fstype == QLatin1String("overlay")) {
         return true;
     }
     return false;
