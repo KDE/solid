316115a3d0b7626999e3ffe354f33e81368d4aee
Nicolas Fella <nicolas.fella@gmx.de>
2026-01-06 18:05:11 +0100

    Add missing signals to StorageAccess backends
    Amends 2313e2d26c0fdf3899110897d85918d558136fa2
    
    BUG: 514176

--- src/solid/devices/backends/fakehw/fakestorageaccess.h
+++ src/solid/devices/backends/fakehw/fakestorageaccess.h
@@ -39,6 +39,10 @@ Q_SIGNALS:
     void teardownDone(Solid::ErrorType error, QVariant errorData, const QString &udi) override;
     void setupRequested(const QString &udi) override;
     void teardownRequested(const QString &udi) override;
+    void checkRequested(const QString &udi) override;
+    void checkDone(Solid::ErrorType error, QVariant errorData, const QString &udi) override;
+    void repairRequested(const QString &udi) override;
+    void repairDone(Solid::ErrorType error, QVariant errorData, const QString &udi) override;
 
 private Q_SLOTS:
     void onPropertyChanged(const QMap<QString, int> &changes);
