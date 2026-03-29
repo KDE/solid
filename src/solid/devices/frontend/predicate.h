347eae4724f87663faf2a4b207ac11e83a7c947b
Thiago Sueto <herzenschein@gmail.com>
2026-03-28 21:21:27 -0300

    QDoc Predicate

--- src/solid/devices/frontend/predicate.h
+++ src/solid/devices/frontend/predicate.h
@@ -30,11 +30,16 @@ class Device;
  * interface, or any combination (conjunction, disjunction) of such
  * constraints.
  *
+ * Predicates are used both in code and in Solid action files
+ * (present in \c /usr/share/solid/actions or \c ~/.local/share/solid/actions).
+ * When used in Solid action files, they must be present in the \c X-KDE-Solid-Predicate=
+ * key in the Desktop Entry section of the file.
+ *
  * A predicate can be:
  * \list
- * \li a single comparison, or
- * \li a conjunction ("AND") of exactly two predicates, or
- * \li a disjunction ("OR") of exactly two predicates.
+ * \li a single comparison
+ * \li a conjunction of exactly two predicates ("AND")
+ * \li a disjunction of exactly two predicates ("OR")
  * \endlist
  *
  * Since these can be nested, it is possible to express "a StorageVolume
@@ -49,15 +54,17 @@ class Device;
  * \endcode
  *
  * Predicates can be constructed programmatically by creating single
- * comparisons with the Predicate constructor, and then building
+ * comparisons with the \l Predicate() constructor, and then building
  * conjunctions with \c operator& and disjunctions with \c operator|.
  *
  * Predicates can be constructed from a string by calling fromString()
- * which parses the given string and returns a predicate. If there
+ * which returns a predicate. If there
  * are any errors in parsing the string, an empty predicate is returned;
  * use isValid() to detect whether that is the case.
  *
- * The string language is described exactly in \c predicate_parser.y,
+ * The string language is described exactly in
+ * \l {https://invent.kde.org/frameworks/solid/-/blob/master/src/solid/devices/predicate_parser.y}
+ * {predicate_parser.y},
  * but boils down to:
  * \list
  * \li a single comparison is written as \c {<interface>.<property> == <value>}
@@ -66,6 +73,7 @@ class Device;
  * \li a disjunction is written as \c {[ <predicate> OR <predicate> ]}
  * \endlist
  * Note the mandatory use of \c {[} and \c {]} around conjunctions and disjunctions.
+ * \sa {https://develop.kde.org/docs/features/solid/}{Solid tutorial}
  */
 class SOLID_EXPORT Predicate
 {
@@ -73,17 +81,17 @@ public:
     /*!
      * The comparison operator which can be used for matching within the predicate.
      *
-     * \value Equals the property and the value will match for strict equality
-     * \value Mask the property and the value will match if the bitmasking is not null
+     * \value Equals The property and the value will match for strict equality
+     * \value Mask The property and the value will match if the bitmasking is not null
      */
     enum ComparisonOperator { Equals, Mask };
 
     /*!
-     * The predicate type which controls how the predicate is handled
+     * The predicate type which controls how the predicate is handled.
      *
-     * \value PropertyCheck the predicate contains a comparison that needs to be matched using a ComparisonOperator
-     * \value Conjunction the two contained predicates need to be true for this predicate to be true
-     * \value Disjunction either of the two contained predicates may be true for this predicate to be true
+     * \value PropertyCheck The predicate contains a comparison that needs to be matched using a ComparisonOperator
+     * \value Conjunction The two contained predicates need to be true for this predicate to be true
+     * \value Disjunction Either of the two contained predicates may be true for this predicate to be true
      * \value InterfaceCheck the device type is compared
      */
     enum Type { PropertyCheck, Conjunction, Disjunction, InterfaceCheck };
@@ -96,49 +104,33 @@ public:
     Predicate(const Predicate &other);
 
     /*!
-     * Constructs a predicate matching the value of a property in
-     * a given device interface.
+     * Constructs a predicate matching the \a value of a \a property in
+     * a given device interface \a ifaceType.
      *
-     * \a ifaceType the device interface type the device must have
-     *
-     * \a property the property name of the device interface
-     *
-     * \a value the value the property must have to make the device match
-     *
-     * \a compOperator the operator to apply between the property and the value when matching
+     * The optional \a compOperator applies between the property and the value when matching.
      */
     Predicate(const DeviceInterface::Type &ifaceType, const QString &property, const QVariant &value, ComparisonOperator compOperator = Equals);
 
     /*!
-     * Constructs a predicate matching the value of a property in
-     * a given device interface.
+     * Constructs a predicate matching the \a value of a \a property in
+     * a given device interface \a ifaceType.
      *
-     * \a ifaceName the name of the device interface the device must have
-     *
-     * \a property the property name of the device interface
-     *
-     * \a value the value the property must have to make the device match
-     *
-     * \a compOperator the operator to apply between the property and the value when matching
+     * The optional \a compOperator applies between the property and the value when matching.
      */
     Predicate(const QString &ifaceName, const QString &property, const QVariant &value, ComparisonOperator compOperator = Equals);
 
     /*!
-     * Constructs a predicate matching devices being of a particular device interface
-     *
-     * \a ifaceType the device interface the device must have
+     * Constructs a predicate matching devices of a particular device interface \a ifaceType.
      */
     explicit Predicate(const DeviceInterface::Type &ifaceType);
 
     /*!
-     * Constructs a predicate matching devices being of a particular device interface
-     *
-     * \a ifaceName the name of the device interface the device must have
+     * Constructs a predicate matching devices of a particular device interface \a ifaceName.
      */
     explicit Predicate(const QString &ifaceName);
 
     /*!
-     * Argument for Predicate constructor.
+     * Argument for the \l Predicate() constructor.
      *
      * \value NOT
      */
@@ -147,12 +139,9 @@ public:
     };
 
     /*!
-     * Constructs a predicate that does a negated check of a device being of a particular device interface.
+     * Constructs a predicate that checks that a device does \b not have the \a ifaceName.
      *
-     * \a ifaceName the name of the device interface the device should not have
-     *
-     * \a op is always NOT
-     *
+     * The operator \a op is always NOT.
      * \since 6.17
      */
     explicit Predicate(QStringView ifaceName, NotOperator op);
@@ -164,147 +153,117 @@ public:
     /*!
      * 'And' operator.
      *
-     * \a other the second operand
-     *
-     * Returns a new 'and' predicate having 'this' and 'other' as operands
+     * Returns a new 'and' predicate having this and \a other as operands.
      */
     Predicate operator&(const Predicate &other);
 
     /*!
      * 'AndEquals' operator.
      *
-     * \a other the second operand
-     *
-     * Assigns to 'this' a new 'and' predicate having 'this' and 'other' as operands
+     * Assigns to this a new 'and' predicate having this and \a other as operands.
      */
     Predicate &operator&=(const Predicate &other);
 
     /*!
      * 'Or' operator.
      *
-     * \a other the second operand
-     *
-     * Returns a new 'or' predicate having 'this' and 'other' as operands
+     * Returns a new 'or' predicate having this and \a other as operands.
      */
     Predicate operator|(const Predicate &other);
 
     /*!
      * 'OrEquals' operator.
      *
-     * \a other the second operand
-     *
-     * Assigns to 'this' a new 'or' predicate having 'this' and 'other' as operands
+     * Assigns to this a new 'or' predicate having this and \a other as operands.
      */
     Predicate &operator|=(const Predicate &other);
 
     /*!
-     * Indicates if the predicate is valid.
+     * Returns if the predicate is valid.
      *
      * Predicate() is the only invalid predicate.
-     *
-     * Returns true if the predicate is valid, false otherwise
      */
     bool isValid() const;
 
     /*!
-     * Checks if a device matches the predicate.
-     *
-     * \a device the device to match against the predicate
-     *
-     * Returns true if the given device matches the predicate, false otherwise
+     * Returns if a \a device matches the predicate.
      */
     bool matches(const Device &device) const;
 
     /*!
-     * Retrieves the device interface types used in this predicate.
-     *
-     * Returns all the device interface types used in this predicate
+     * Returns the device interface types used in this predicate.
      */
     QSet<DeviceInterface::Type> usedTypes() const;
 
     /*!
-     * Converts the predicate to its string form.
-     *
-     * Returns a string representation of the predicate
+     * Returns a string representation of the predicate.
      */
     QString toString() const;
 
     /*!
-     * Converts a string to a predicate.
-     *
-     * \a predicate the string to convert
-     *
-     * Returns a new valid predicate if the given string is syntactically
-     * correct, Predicate() otherwise
+     * Returns a new valid predicate if the given \a predicate string is syntactically
+     * correct, Predicate() otherwise.
+     * \code
+     * Solid::Predicate equalPredicate =
+     *     Solid::Predicate::fromString("IS OpticalDisc");
+     * \endcode
      */
     static Predicate fromString(const QString &predicate);
 
     /*!
-     * Retrieves the predicate type, used to determine how to handle the predicate
-     *
+     * Retrieves the predicate type, used to determine how to handle the predicate.
      * \since 4.4
-     *
-     * Returns the predicate type
      */
     Type type() const;
 
     /*!
-     * Retrieves the interface type.
-     *
-     * \note This is only valid for InterfaceCheck and PropertyCheck types
+     * Returns a device interface type used by the predicate.
+     * \note This is only valid for InterfaceCheck and PropertyCheck types.
      * \since 4.4
-     * Returns a device interface type used by the predicate
      */
     DeviceInterface::Type interfaceType() const;
 
     /*!
-     * Retrieves the property name used when retrieving the value to compare against
-     *
-     * \note This is only valid for the PropertyCheck type
+     * Returns the property name used when retrieving the value to compare against.
+     * \note This is only valid for the PropertyCheck type.
      * \since 4.4
-     * Returns a property name
      */
     QString propertyName() const;
 
     /*!
-     * Retrieves the value used when comparing a devices property to see if it matches the predicate
-     *
-     * \note This is only valid for the PropertyCheck type
+     * Returns the value used when comparing a devices property to see if it matches the predicate.
+     * \note This is only valid for the PropertyCheck type.
      * \since 4.4
-     * Returns the value used
      */
     QVariant matchingValue() const;
 
     /*!
-     * Retrieves the comparison operator used to compare a property's value
-     *
+     * Returns the comparison operator used to compare a property's value.
      * \since 4.4
-     * \note This is only valid for Conjunction and Disjunction types
-     * Returns the comparison operator used
+     * \note This is only valid for Conjunction and Disjunction types.
      */
     ComparisonOperator comparisonOperator() const;
 
     /*!
-     * A smaller, inner predicate which is the first to appear and is compared with the second one
+     * A smaller, inner predicate which is the first to appear and is compared
+     * with the second one in a comparison operation.
      *
      * \since 4.4
-     * \note This is only valid for Conjunction and Disjunction types
-     * Returns The predicate used for the first operand
+     * \note This is only valid for Conjunction and Disjunction types.
      */
     Predicate firstOperand() const;
 
     /*!
-     * A smaller, inner predicate which is the second to appear and is compared with the first one
+     * A smaller, inner predicate which is the second to appear and is compared
+     * with the first one in a comparison operation.
      *
      * \since 4.4
-     * \note This is only valid for Conjunction and Disjunction types
-     * Returns The predicate used for the second operand
+     * \note This is only valid for Conjunction and Disjunction types.
      */
     Predicate secondOperand() const;
 
     /*!
-     * True if the interface type check is negated.
-     *
+     * Returns if the interface type check is negated.
      * \since 6.17
      */
     bool isInterfaceTypeNegated() const;
